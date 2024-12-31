afebe76ceaf80fc6540d271378a061018f1a1ae2
Diogo Fialho <diogo.fialho@gmail.com>
2024-12-31 12:10:19 +0000

    setting up cleanup db backup logic on backup sh script

--- Backup/backup.sh
+++ Backup/backup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 TIMESTAMP=$(date +"%F_%T")
-BACKUP_DIR="/backup"
+BACKUP_DIR="$1"
+DAYS_OLD=$3
 mkdir -p "$BACKUP_DIR"
-mongodump --host=mongodb --out="$BACKUP_DIR/$TIMESTAMP" -d osrs_api
+
+echo "creating backup"
+mongodump --host=$2 --out="$BACKUP_DIR/$TIMESTAMP"
+
+echo "cleaning up older files"
+find $BACKUP_DIR -mindepth 1 -maxdepth 1 -type d -mtime +$DAYS_OLD -exec sh -c 'echo Removing: {}; rm -rf {}' \;
